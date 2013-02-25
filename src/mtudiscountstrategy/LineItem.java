6fa494e14436af467e9db31267aa536ab418df47
murbanski@my.wctc.edu <>
2013-02-25 00:47:30 -0600

    Added constructors to PercentOffDiscount. Copied in FakeDatabase from example. Added properties and setters/getters to LineItem.

--- src/mtudiscountstrategy/LineItem.java
+++ src/mtudiscountstrategy/LineItem.java
@@ -5,10 +5,37 @@ package mtudiscountstrategy;
  * @author Mark Urbanski
  */
 public class LineItem {
+    private String productId;
+    private int quantity;
     private Product product;
-    
-    
-    
-    
+
+    public LineItem(String productId, int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
     
 }
--- src/mtudiscountstrategy/PercentOffDiscount.java
+++ src/mtudiscountstrategy/PercentOffDiscount.java
@@ -8,6 +8,16 @@ public class PercentOffDiscount implements DiscountStrategy {
     private double discountRate = .20;
     //private double price = 20.00;
     //private int qty = 3;
+
+    public PercentOffDiscount() {
+        //don't do anything
+    }
+
+    public PercentOffDiscount(double discountRate) {
+        this.discountRate = discountRate;
+    }
+    
+    
     
     @Override
     public double getDiscount(int qty, double price) {
