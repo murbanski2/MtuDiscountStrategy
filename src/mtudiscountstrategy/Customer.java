a615b1d1fac8f4bafb207922b183112759f47f50
murbanski@my.wctc.edu <>
2013-02-25 00:51:40 -0600

    Added Customer with constructor, getters and setters.

---
+++ src/mtudiscountstrategy/Customer.java
@@ -0,0 +1,34 @@
+package mtudiscountstrategy;
+
+/**
+ *
+ * @author Mark Urbanski
+ */
+public class Customer {
+    private String custId;
+    private String custName;
+
+    public Customer(String custId, String custName) {
+        this.custId = custId;
+        this.custName = custName;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+    
+    
+    
+}
