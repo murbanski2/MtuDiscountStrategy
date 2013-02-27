4f8b7585aa6dc3424a0b78693d23cc98c577ff9f
murbanski@my.wctc.edu <>
2013-02-26 22:34:56 -0600

    Added accumulators and functions to do the accumulating.

--- src/mtudiscountstrategy/Receipt.java
+++ src/mtudiscountstrategy/Receipt.java
@@ -8,6 +8,18 @@ public class Receipt {
     private LineItem[] lineItem = new LineItem[0];
     private Customer customer;
     
+    private double totalFullPrice;
+    private double totalDiscount;
+    
+    private void addToTotalFullPrice(double amount) {
+        //testDouble
+        totalFullPrice += amount;
+    }
+    
+    private void addToTotalDiscount(double amount) {
+        //testDouble
+        totalDiscount += amount;
+    }
     
     
 }
