213ef127cd724d293bc8909b43244f5b390eba4b
murbanski@my.wctc.edu <>
2013-03-03 14:06:22 -0600

    Added functions for Receipt, using latest LineItem.  Need to add output.

--- src/mtudiscountstrategy/Receipt.java
+++ src/mtudiscountstrategy/Receipt.java
@@ -5,51 +5,66 @@ package mtudiscountstrategy;
  * @author murbanski
  */
 public class Receipt {
-    private LineItem[] lineItem = new LineItem[0];
+    private LineItem[] lineItems = new LineItem[0];
     private Customer customer;
     
-    private double totalFullPrice;
-    private double totalDiscount;
 
-    public Receipt(String custId, String productId, int qty) {
-        boolean errorFound = false;
-        FakeDatabase fakeDatabase = new FakeDatabase();
-        Customer cust = fakeDatabase.findCustomer(custId);
-        Product prod = fakeDatabase.findProduct(productId);
-        if(cust == null) {
-            errorFound = true;
-            showError("Customer not found");
-        }
-        if(prod == null) {
-            errorFound = true;
-            showError("Product not found");
-        }
-        
-        if(!errorFound) {
-            customer = cust;
-            
-        }
+
+    public Receipt(String custId) {
+        setCustomer(custId);
     }
     
+    public Customer getCustomer(String customerId) {
+        FakeDatabase fakeDatabase = new FakeDatabase();
+        customer = fakeDatabase.findCustomer(customerId);
+        return customer;
+    }
+    
+    public void setCustomer(String customerId) {
+        FakeDatabase fakeDatabase = new FakeDatabase();
+        Customer cust = fakeDatabase.findCustomer(customerId);
+        if(cust == null) {
+            showError("Customer not found");
+        }
+        else {
+            customer = cust;
+        }
+    }
 
 
     public void addLineItem(Product product, int qty) {
+        //From CodeSamples
         LineItem item = new LineItem(product, qty);
         addToArray(item);
     }
     
-    public void addToArray(LineItem item) {
-    
+
+    private void addToArray(LineItem item) {
+        //From CodeSamples
+        LineItem[] tempItems = new LineItem[lineItems.length + 1];
+        System.arraycopy(lineItems, 0, tempItems, 0, lineItems.length);
+        tempItems[lineItems.length] = item;
+        lineItems = tempItems;
     }
+
+
+    public double getTotalBeforeDiscount() {
+        //from CodeSamples
+        //Loop thru the LineItem array and add the extended prices
+        double grandTotal = 0.0;
+        for(LineItem item : lineItems) {
+            grandTotal += item.getExtendedPrice();
+        }
+        return grandTotal;
+    }    
     
-    private void addToTotalFullPrice(double amount) {
-        //testDouble
-        totalFullPrice += amount;
-    }
-    
-    private void addToTotalDiscount(double amount) {
-        //testDouble
-        totalDiscount += amount;
+    public double getTotalDiscount() {
+        //Loop thru the LineItem array and add the discounts
+        double totalDiscount = 0.0;
+        for(LineItem item : lineItems) {
+            totalDiscount += item.getDiscount();
+        }
+        return totalDiscount;
     }
     
     private void showError(String error ) {
@@ -58,3 +73,5 @@ public class Receipt {
     }
             
 }
+
+
