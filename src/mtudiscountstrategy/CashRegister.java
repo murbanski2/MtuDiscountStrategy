573346ff60f329583103e8db29ea639638c1fa75
murbanski@my.wctc.edu <>
2013-03-03 15:46:20 -0600

    First try at CashRegister.  This one should be pretty simple. StartSale will create a new receipt based on customerID. addLineItem will pass productID and quantity thru to the receipt. endSale will get the receipt.

---
+++ src/mtudiscountstrategy/CashRegister.java
@@ -0,0 +1,21 @@
+package mtudiscountstrategy;
+
+/**
+ *
+ * @author Mark Urbanski
+ */
+public class CashRegister {
+    private Receipt receipt;
+    
+    public void startSale(String custID) {
+        
+    }
+    
+    public void addLineItem(String prodId, int quantity) {
+    
+    }
+    
+    public void endSale() {
+        receipt.getReceipt();
+    }
+}
