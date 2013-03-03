9d8c72705b6799c85662f9c8f0083ce70ae14867
murbanski@my.wctc.edu <>
2013-03-03 16:11:41 -0600

    startSale() is fleshed out. scanItem replaces my addLineItem() because a Cash Register would scan items in, not create a line item. Test method added.

--- src/mtudiscountstrategy/CashRegister.java
+++ src/mtudiscountstrategy/CashRegister.java
@@ -7,15 +7,25 @@ package mtudiscountstrategy;
 public class CashRegister {
     private Receipt receipt;
     
-    public void startSale(String custID) {
-        
+    public void startSale(String custId) {
+        receipt = new Receipt(custId);
     }
     
-    public void addLineItem(String prodId, int quantity) {
-    
+    public void scanItem(String prodId, int quantity) {
+        receipt.addItemToSale(prodId, quantity);
     }
     
     public void endSale() {
         receipt.getReceipt();
+    }
+    
+    public static void main(String[] args) {
+        CashRegister cr = new CashRegister();
+        cr.startSale("200");
+        cr.scanItem("C222", 6);
+        cr.scanItem("B205", 4);
+        cr.scanItem("A101", 16);
+        cr.endSale();
+        
     }
 }
