a34c0775fe32c59c8624ec22422be7c9151bf5e2
murbanski@my.wctc.edu <>
2013-03-03 17:12:38 -0600

    Finished startup classw

--- src/mtudiscountstrategy/Startup.java
+++ src/mtudiscountstrategy/Startup.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mtudiscountstrategy;
 
 /**
@@ -14,6 +10,28 @@ public class Startup {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Do first cash register
+        CashRegister cr1 = new CashRegister();
+        cr1.startSale("200");
+        cr1.scanItem("C222", 6);
+        cr1.scanItem("B205", 4);
+        cr1.scanItem("A101", 16);
+        cr1.endSale();
+        
+        //Do second cash register
+        CashRegister cr2 = new CashRegister();
+        cr2.startSale("300");
+        cr2.scanItem("C222", 2);
+        cr2.scanItem("B205", 5);
+        cr2.scanItem("A101", 5);
+        cr2.endSale();
+        
+        //Do third cash register
+        CashRegister cr3 = new CashRegister();
+        cr3.startSale("100");
+        cr3.scanItem("C222", 1);
+        cr3.scanItem("B205", 6);
+        cr3.scanItem("A101", 6);
+        cr3.endSale();
     }
 }
