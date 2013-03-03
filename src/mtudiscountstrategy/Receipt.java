dafa19ae19c342e646b8dc50364960963f06ab98
murbanski@my.wctc.edu <>
2013-03-03 11:46:52 -0600

    Working on Receipt class.  I can see that I am already doing to much with the constructor.  I need to put my code from the CodeSamples into the class.  I think a receipt can be constructed with just a customer.  The product only needs to be validated when I add a LineItem.  But I want to take a snapshot of this so I don't lose anything.

--- src/mtudiscountstrategy/Receipt.java
+++ src/mtudiscountstrategy/Receipt.java
@@ -10,6 +10,37 @@ public class Receipt {
     
     private double totalFullPrice;
     private double totalDiscount;
+
+    public Receipt(String custId, String productId, int qty) {
+        boolean errorFound = false;
+        FakeDatabase fakeDatabase = new FakeDatabase();
+        Customer cust = fakeDatabase.findCustomer(custId);
+        Product prod = fakeDatabase.findProduct(productId);
+        if(cust == null) {
+            errorFound = true;
+            showError("Customer not found");
+        }
+        if(prod == null) {
+            errorFound = true;
+            showError("Product not found");
+        }
+        
+        if(!errorFound) {
+            customer = cust;
+            
+        }
+    }
+    
+
+
+    public void addLineItem(Product product, int qty) {
+        LineItem item = new LineItem(product, qty);
+        addToArray(item);
+    }
+    
+    public void addToArray(LineItem item) {
+    
+    }
     
     private void addToTotalFullPrice(double amount) {
         //testDouble
@@ -21,5 +52,9 @@ public class Receipt {
         totalDiscount += amount;
     }
     
-    
+    private void showError(String error ) {
+        //I plan to do this with a popup later
+        System.out.println(error + " in Receipt");
+    }
+            
 }
