33bf9c1ba819bb8572708b1b9afc45df964d8db5
murbanski@my.wctc.edu <>
2013-03-03 21:30:54 -0600

    Made product array in FakeDatabase private.

--- src/mtudiscountstrategy/FakeDatabase.java
+++ src/mtudiscountstrategy/FakeDatabase.java
@@ -14,7 +14,7 @@ public class FakeDatabase {
     };
 
 // Notice we assign a default discount strategy component object to each product
-    Product[] products = {
+    private Product[] products = {
         new Product("A101", "Baseball Hat", 19.95, new PercentOffDiscount(0.15)),
         new Product("B205", "Men's Dress Shirt", 35.50, new QtyDiscount(.10,5)),
         new Product("C222", "Women's Socks", 9.50, new NoDiscount())
